30084788ce1f0d7da8a2300540d2e7c401d2ef97
PolCode33 <hvf01010@gmail.com>
2021-01-04 11:15:55 -0500

    Primer Commit de prueba

---
+++ app/src/main/java/com/explode/miprimeraapp/InterfaceToast.java
@@ -0,0 +1,5 @@
+package com.explode.miprimeraapp;
+
+public interface InterfaceToast {
+    void mensaje(String mensaje);
+}
---
+++ app/src/main/java/com/explode/miprimeraapp/Manipulacion.java
@@ -0,0 +1,51 @@
+package com.explode.miprimeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Manipulacion extends AppCompatActivity {
+    EditText edtnombre;
+    EditText edtmonto;
+    TextView txtresultado;
+    Button btnejecutar;
+    Button btnver;
+    Boolean centinela=true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manipulacion);
+        edtnombre=findViewById(R.id.edtnombre);
+        edtmonto=findViewById(R.id.edtmonto);
+        txtresultado=findViewById(R.id.txtResultado);
+        btnejecutar=findViewById(R.id.btncambiar);
+        btnver= findViewById(R. id. btnver);
+        btnejecutar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double cambio;
+                cambio=Double.parseDouble(edtmonto.getText().toString())*3.3;
+                txtresultado.setText(edtnombre.getText() + " el monto de "+ edtmonto.getText()+
+                        " dolares equivale a " + cambio.toString() + "Soles");
+            }
+        });
+    }
+
+    public void ocultar(View view) {
+        if (centinela) {
+            btnejecutar.setVisibility(View.GONE);
+            btnver.setText("mostrar");
+            centinela=false;
+        }
+        else
+        {
+            btnejecutar.setVisibility(View.VISIBLE);
+            btnver.setText("ocultar");
+            centinela=true;
+        }
+    }
+}
